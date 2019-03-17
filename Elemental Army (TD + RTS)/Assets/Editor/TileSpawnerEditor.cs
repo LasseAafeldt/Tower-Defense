03a03026249865ebe1b4842909e2994d6f5e0907
Lasse Aafeldt <superlla@hotmail.com>
2019-03-17 12:49:37 +0100

    Renamed GridGenerator to TileSpawner - TileSpawner math have been corrected so it now spawns tiles in the correct positions - added ability to spawn tiles with parent as center of grid. added textMesh pro asset.

---
+++ Elemental Army (TD + RTS)/Assets/Editor/TileSpawnerEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TileSpawner))]
+public class TileSpawnerEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        TileSpawner tile = (TileSpawner)target;
+        
+        if (GUILayout.Button("Generate Grid"))
+        {          
+            tile.overwriteGrid();
+        }       
+    }
+}
---
+++ Elemental Army (TD + RTS)/Assets/Scripts/TileSpawner.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+public class TileSpawner : MonoBehaviour {
+    //public bool Overwrite;
+    public GameObject gridParent;
+    public bool parentIsCenter = false;
+
+    [Header("Grid Tile look")]
+    public GameObject gridTilePrefab;
+
+    [Header("Grid size")]
+    public int sizeX;
+    public int sizeZ;
+    public float tileSpace;
+    private float posY;
+
+    private Vector3 parentPos;
+
+    public void generateGrid()
+    {
+        parentPos = gridParent.transform.position;
+        posY = parentPos.y;
+        if (!gridTilePrefab.GetComponent<Renderer>())
+        {
+            Debug.LogWarning("Grid tile prefab is missing Renderer");
+        }
+
+        //float xOffset = gridTilePrefab.GetComponent<Renderer>().bounds.size.x;
+        float boundsSizeZ = gridTilePrefab.GetComponent<Renderer>().bounds.size.z;
+        float boundsSizeX = gridTilePrefab.GetComponent<Renderer>().bounds.size.x;
+        float xOffset = gridTilePrefab.GetComponent<Renderer>().bounds.size.x;
+        float zOffset = gridTilePrefab.GetComponent<Renderer>().bounds.size.z;
+
+        if (!parentIsCenter)
+        {
+            for (int i = 0; i < sizeX; i++)
+            {
+                //xOffset += tileSpace;
+                for (int j = 0; j < sizeZ; j++)
+                {                   
+                    Vector3 position = new Vector3(i * (boundsSizeX + tileSpace) + parentPos.x, 
+                        posY, 
+                        j * (boundsSizeZ + tileSpace) + parentPos.z);
+                    GameObject tile = Instantiate(gridTilePrefab, position, transform.rotation, gridParent.transform);
+                }
+            }
+        }
+        
+        if (parentIsCenter)
+        {
+            Vector3 cornerPos = new Vector3(parentPos.x - Mathf.Abs(((sizeX/2) * boundsSizeX) +(tileSpace * (sizeX/2))), 
+                parentPos.y, 
+                parentPos.z - Mathf.Abs(((sizeZ/2) * boundsSizeZ) + (tileSpace * (sizeZ/2))));            
+            for (int i = 0; i < sizeX; i++)
+            {               
+                for (int j = 0; j < sizeZ; j++)
+                {
+                    Vector3 position = new Vector3(i * (boundsSizeX + tileSpace) + cornerPos.x,
+                        posY,
+                        j * (boundsSizeZ + tileSpace) + cornerPos.z);
+                    GameObject tile = Instantiate(gridTilePrefab, position, transform.rotation, gridParent.transform);
+                }
+            }
+        }
+    }
+
+    public void overwriteGrid()
+    {
+        deleteGrid();
+        generateGrid();
+    }
+
+    public void deleteGrid()
+    {
+        while (gridParent.transform.childCount > 0)
+        {
+            for (int i = 0; i < gridParent.transform.childCount; i++)
+            {
+                DestroyImmediate(gridParent.transform.GetChild(0).gameObject);
+            }
+        }
+        //gridParent.transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+    }
+}
